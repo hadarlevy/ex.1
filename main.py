ae78aa4ad9c7782f5e2516b8666708e91ae444d3
hadarlevy <94686791+hadarlevy@users.noreply.github.com>
2022-03-06 19:55:27 +0200

    ver1.1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
-import math
+import sys
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -12,6 +13,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print(Abs (3.0*(4.0/3.0-1)-1))
+    print(sys.float_info.epsilon-abs(3.0*(4.0/3.0-1)-1))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
